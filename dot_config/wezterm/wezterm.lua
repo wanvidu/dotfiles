f0516eb4e9b3de54cd63ad567e19a83c9db775c7
wanvidu <wanvidu@gmail.com>
2025-11-10 08:18:37 +0530

    Config Updated.

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -6,59 +6,92 @@
 -- Rectangular selection 	Ctrl + V
 
 local wezterm = require("wezterm")
-
+local mux = wezterm.mux
+local act = wezterm.action
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("FiraCode Nerd Font Mono")
-config.font_size = 14
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 15
 
 config.window_decorations = "RESIZE"
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.default_prog = { "pwsh.exe" }
+	config.default_prog = { "ubuntu.exe" }
 end
+
+local launch_menu = {}
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	table.insert(launch_menu, {
+		label = "PowerShell",
+		args = { "pwsh.exe" },
+	})
+
+	table.insert(launch_menu, {
+		label = "Ubuntu",
+		args = { "ubuntu.exe" },
+	})
+end
+
+config.launch_menu = launch_menu
 
 -- config.color_scheme = "Adventure"
 
 config.colors = {
-  foreground = '#34AB2A',
-  background = '#000000',
+	foreground = "#34AB2A",
+	background = "#000000",
 
-  cursor_bg = '#CFCFCF',
-  cursor_fg = '#CFCFCF',
-  cursor_border = '#CFCFCF',
+	cursor_bg = "#CFCFCF",
+	cursor_fg = "#CFCFCF",
+	cursor_border = "#CFCFCF",
 
-  selection_fg = '#00A48C',
-  selection_bg = '#00A48C',
+	selection_fg = "#00A48C",
+	selection_bg = "#00A48C",
 
-  scrollbar_thumb = '#969696',
+	scrollbar_thumb = "#969696",
 
-  split = '#969696',
+	split = "#969696",
 
-  ansi = {
-    '#000000',
-    '#9F0000',
-    '#008B00',
-    '#FFD000',
-    '#0081FF',
-    '#BC00CA',
-    '#008B8B',
-    '#969696',
-  },
+	ansi = {
+		"#000000",
+		"#9F0000",
+		"#008B00",
+		"#FFD000",
+		"#0081FF",
+		"#BC00CA",
+		"#008B8B",
+		"#969696",
+	},
 
-  brights = {
-    '#555555',
-    '#FF0000',
-    '#00EE00',
-    '#FFFF00',
-    '#0A5DFF',
-    '#FF00FF',
-    '#00CDCD',
-    '#CCCCCC',
-  },
+	brights = {
+		"#555555",
+		"#FF0000",
+		"#00EE00",
+		"#FFFF00",
+		"#0A5DFF",
+		"#FF00FF",
+		"#00CDCD",
+		"#CCCCCC",
+	},
+}
+
+config.foreground_text_hsb = {
+	hue = 1.0,
+	saturation = 1.2,
+	brightness = 1.5,
+}
+
+config.keys = {
+	{ key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+	{ key = "Enter", mods = "SHIFT", action = wezterm.action({ SendString = "\x1b\r" }) },
 }
 
 config.mouse_bindings = {
+	{
+		event = { Down = { streak = 3, button = "Left" } },
+		action = wezterm.action.SelectTextAtMouseCursor("SemanticZone"),
+		mods = "NONE",
+	},
 	{
 		event = { Down = { streak = 1, button = "Right" } },
 		mods = "NONE",
@@ -75,4 +108,3 @@ config.mouse_bindings = {
 }
 
 return config
-
